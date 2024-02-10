a1183c2f3ca19a5dbb39e1e8263251b257a1fb6a
WestWild <miroqaan@gmail.com>
2024-02-10 16:29:40 +0900

    -

--- src/baekjoon_2750.java
+++ src/baekjoon_2750.java
@@ -1,29 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class baekjoon_2750 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N];
+        mData[] A = new mData[N];
 
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
+            A[i] = new mData(Integer.parseInt(br.readLine()), i);
         }
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        Arrays.sort(A); // A 배열 정렬(O(nlogn) 시간 복잡도)
+        int Max = 0;
+        for (int i = 0; i < N; i++) {
+            if (Max < A[i].index - i) {
+                Max = A[i].index - i; // 정렬 전 index - 정렬 후 index의 최댓값 저장
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(nums[i]);
-        }
+        System.out.println(Max + 1);
+    }
+}
+
+class mData implements Comparable<mData> {
+    int value;
+    int index;
+
+    public mData(int value, int index) {
+        super();
+        this.value = value;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(mData o) {
+        return this.value - o.value; // 오름차순 정렬
     }
 }
