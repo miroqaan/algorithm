1895770d581cdb955caa9a27185ad87638fa2b4b
WestWild <miroqaan@gmail.com>
2024-03-17 12:07:28 +0900

    병합정렬 오류 수정

--- src/P2751.java
+++ src/P2751.java
@@ -9,7 +9,7 @@ public class P2751 {
         int N = Integer.parseInt(br.readLine());
         A = new int[N + 1];
         tmp = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
         merge_sort(1, N);   // 병합 정렬 수행하기
@@ -36,13 +36,13 @@ public class P2751 {
             // 양쪽 그룹의 index가 가리키는 값을 비교해 더 작은 수를 선택해 배열에 저장하고,
             // 선택된 데이터의 index 값을 오른쪽으로 한 칸 이동하기
             if (tmp[index1] <= tmp[index2]) {
-                A[k] = tmp[index2];
-                k++;
-                index2++;
-            } else {
                 A[k] = tmp[index1];
                 k++;
                 index1++;
+            } else {
+                A[k] = tmp[index2];
+                k++;
+                index2++;
             }
         }
         while (index1 <= m) {   // 한쪽 그룹이 모두 선택된 후 남아 있는 값 정리하기
@@ -50,10 +50,10 @@ public class P2751 {
             k++;
             index1++;
         }
-        while (index2 <= e) {
-            A[k] = tmp[index2];
-            k++;
-            index2++;
-        }
+//        while (index2 <= e) {
+//            A[k] = tmp[index2];
+//            k++;
+//            index2++;
+//        }
     }
 }
