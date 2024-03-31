39dc19576b15d299d83e83c8e2cf547e67433849
WestWild <miroqaan@gmail.com>
2024-03-31 20:30:08 +0900

    -

---
+++ src/P10989.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+
+    }
+
+    public static int sort(){
+
+
+        return 0;
+    }
+}
